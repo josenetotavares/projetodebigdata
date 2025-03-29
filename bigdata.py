1ea75acbb1608e277d2d8cd7300ec8df78ba51c6
José Angelo <jtavares197u@gmail.com>
2025-03-29 09:20:30 -0300

    modificação do projeto

--- bigdata.py
+++ bigdata.py
@@ -1,9 +1,22 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
-caminho_arquivo = ("CarrosEletricos.xlsx.xlsx")
-tabela = pd.read_excel(caminho_arquivo)
+caminho_arquivo = r"C:\Users\José Angelo\Documents\projetodebigdata\taxa_rendimento_territorios-29-2023-EM.xlsx"
+tabela = pd.read_excel(caminho_arquivo, sheet_name="estado")
 
+tabela = tabela.dropna(subset=["aprovados","reprovados","abandonos"])
 
-print(tabela)
+df_grouped = tabela.groupby("dependencia_id")[["aprovados","reprovados","abandonos"]].mean()
+
+df_grouped.plot(kind="bar", stacked=True, colormap="viridis", figsize=(10,6))
+
+plt.xlabel("Denpendência ID")
+plt.ylabel("Percentual (%)")
+plt.title("Taxa De Aprovados, Reprovados e Abandonos por Dependência ID")
+plt.legend(title="Categoria")
+plt.xticks(rotation=0)
+
+plt.show(block=True)
+
